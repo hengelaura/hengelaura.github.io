fc96434c722d5ca45bb8af946e41ef40f0c90b27
Laura Hengel <hengelaura@gmail.com>
2024-08-13 13:26:48 -0500

    trying to add framework for website

---
+++ portfolio.py
@@ -0,0 +1,7 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(loader=PackageLoader('portfolio'),
+                  autoescape=select_autoescape())
+
+template = env.get_template('index.html')
+print(template.render())
